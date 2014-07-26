fdf62b2df6802d51b8c8893958a1494ed43b9c0b
Asfanur <asfanur.arafin@gmail.com>
2014-07-26 13:59:11 +1000

    added network manager

---
+++ battlehack1/NetworkManager.h
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.h
+//  battlehack1
+//
+//  Created by Asfanur Arafin on 26/07/2014.
+//  Copyright (c) 2014 Battlehack. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkManager : NSObject
+
+@end
---
+++ battlehack1/NetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.m
+//  battlehack1
+//
+//  Created by Asfanur Arafin on 26/07/2014.
+//  Copyright (c) 2014 Battlehack. All rights reserved.
+//
+
+#import "NetworkManager.h"
+
+@implementation NetworkManager
+
+@end
