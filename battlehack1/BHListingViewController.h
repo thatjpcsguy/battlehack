25a40bae9323a159a7888299e941c6b008e4c870
Jimmy Young <jimmyyoung@live.com.au>
2014-07-26 17:34:50 +1000

    Adding the connections

---
+++ battlehack1/BHListingViewController.h
@@ -0,0 +1,19 @@
+//
+//  BHListingViewController.h
+//  battlehack1
+//
+//  Created by Jimmy Young on 26/07/2014.
+//  Copyright (c) 2014 Battlehack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BHListingViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *priceUILabel;
+@property (strong, nonatomic) IBOutlet UIButton *buyButton;
+@property (strong, nonatomic) IBOutlet UIButton *buyButtonPressed;
+@property (strong, nonatomic) IBOutlet UIImageView *listingImageView;
+
+
+
+@end
